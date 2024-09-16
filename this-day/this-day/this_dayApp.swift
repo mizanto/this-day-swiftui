806e01dcbb182775a4a381c0693732e4b429dbec
Sergey Bendak <sleepoke@gmail.com>
2024-09-16 12:38:54 +0200

    add empty project

---
+++ this-day/this-day/this_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  this_dayApp.swift
+//  this-day
+//
+//  Created by Sergey Bendak on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct this_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
